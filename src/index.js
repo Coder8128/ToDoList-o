eaaeca1a4b1601eccbf7ce89c452edb70e704daa
Coder8128 <57500825+Coder8128@users.noreply.github.com>
2026-07-19 13:50:02 +0200

    Implement basic create element

--- src/index.js
+++ src/index.js
@@ -18,3 +18,31 @@ function createPorject(name) {
         }
     };
 }
+
+document.getElementById("itemForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const todo = createToDo(e.target[0].value, e.target[1].value, e.target[2].value);
+    console.log(todo);
+
+    //Side tab
+    var titl = document.createElement("H1");
+    titl.innerHTML = todo.title;
+    document.querySelector(".tododTitle").appendChild(titl);
+
+    //Right tab
+
+    var desc = document.createElement("p");
+    var prio = document.createElement("p");
+    var titleFull = document.createElement("H2");
+
+    titleFull.innerHTML = todo.title;
+    desc.innerHTML = todo.text;
+    prio.innerHTML = todo.priority;
+
+    document.querySelector(".todoFull").appendChild(titleFull);
+    document.querySelector(".todoFull").appendChild(desc);
+    document.querySelector(".todoFull").appendChild(prio);
+
+
+});
